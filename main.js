5098544fd785cc0d79f2c9d2986a75ba1ecbdbd7
Charin73 <84718244+Charin73@users.noreply.github.com>
2021-10-25 17:25:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+nose_x = 0;
+nose_y = 0;
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function take_snapshot() {
+    save('myFilterImage.png');
+}
+function modelLoaded() {
+    console.log("PoseNet has loaded");
+}
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results)
+        nose_x = results[0].pose.nose.x -15;
+        nose_y = results[0].pose.nose.y -15;
+        console.log("Nose x = "+ nose_x + "Nose y = "+ nose_y);
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+        image(clown_nose, nose_x, nose_y, 30, 30);
+}
+
+function preload() {
+    clown_nose = loadImage('https://i.postimg.cc/pdYTFZGM/Clown-Nose-Image.png');
+}
